8e2ac6d537ae7b2f43a169e94f207a03e7927d53
Sasha <sashazaitsev2005@gmail.com>
2023-07-12 00:11:02 +0300

    Icons

--- TimeTrackerFront/time-tracker-front/src/Login/Features/Login.tsx
+++ TimeTrackerFront/time-tracker-front/src/Login/Features/Login.tsx
@@ -1,12 +1,12 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import * as Yup from 'yup';
-import { Formik} from 'formik';
+import { Formik } from 'formik';
 import type { Login } from '../Models/ViewModels';
-import {Container, Nav, Navbar, NavDropdown, Button, Card, Table, Form } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown, Button, Card, Table, Form } from "react-bootstrap";
 import {
   useNavigate,
 } from "react-router-dom";
-import {ajaxForLoginLogout,getQueryObserver} from "../Api/login-logout";
+import { ajaxForLoginLogout, getQueryObserver } from "../Api/login-logout";
 
 export default function Login() {
 
@@ -14,29 +14,30 @@ export default function Login() {
   const initialValues: Login = { password: "", loginOrEmail: "" };
   const [WrongMessage, setWrongMessage] = useState("");
 
-return (
-    <div className="div-login-form d-flex align-items-center flex-column m-1'">
-    <h5 className=''>Sign in to TimeTracker</h5>
-    <Card style={{ width: '18rem' }} className='d-flex align-items-center flex-column'>
-        <Card.Body className='p-3 w-100'>
-          <Formik
-            validationSchema={Yup.object({
-              password: Yup.string()
-                .required('Required')
-                .min(8, "Password must have min 8 symbols")
-                .max(20, "Password must have max 20 symbols"),
-              loginOrEmail: Yup.string().required('Required')
-            })}
-            initialValues={initialValues}
-            onSubmit={(value: Login, action) => {
-              ajaxForLoginLogout({"login":value}).subscribe(getQueryObserver(setWrongMessage,navigate,"/"))
-              action.setSubmitting(false);
-            }
-            }
-          >{formik => (
-            <Form className="d-flex align-items-start flex-column" onSubmit={(e)=>{
-              e.preventDefault();
-              formik.handleSubmit(e);
+  return (
+    <div className="div-login-form container-fluid p-0 h-100 d-flex  justify-content-center">
+      <div className='d-flex flex-column mt-5'>
+        <h5 className='text-center'>Sign in to TimeTracker</h5>
+        <Card style={{ width: '18rem' }} className='d-flex align-items-center flex-column justify-content-center'>
+          <Card.Body className='p-3 w-100'>
+            <Formik
+              validationSchema={Yup.object({
+                password: Yup.string()
+                  .required('Required')
+                  .min(8, "Password must have min 8 symbols")
+                  .max(20, "Password must have max 20 symbols"),
+                loginOrEmail: Yup.string().required('Required')
+              })}
+              initialValues={initialValues}
+              onSubmit={(value: Login, action) => {
+                ajaxForLoginLogout({ "login": value }).subscribe(getQueryObserver(setWrongMessage, navigate, "/"))
+                action.setSubmitting(false);
+              }
+              }
+            >{formik => (
+              <Form className="d-flex align-items-start flex-column" onSubmit={(e) => {
+                e.preventDefault();
+                formik.handleSubmit(e);
               }}>
                 <p className='m-0'>Username or email adress</p>
                 <Form.Control
@@ -44,7 +45,7 @@ return (
                   className="w-100 mb-3"
                   {...formik.getFieldProps('loginOrEmail')}
                 />
-              {formik.touched.loginOrEmail && formik.errors.loginOrEmail ? (
+                {formik.touched.loginOrEmail && formik.errors.loginOrEmail ? (
                   <p className='error'>{formik.errors.loginOrEmail}</p>
                 ) : null}
                 <p className='m-0'>Password</p>
@@ -53,20 +54,21 @@ return (
                   className="w-100 mb-3"
                   {...formik.getFieldProps('password')}
                 />
-              {formik.touched.password && formik.errors.password ? (
+                {formik.touched.password && formik.errors.password ? (
                   <p className='error'>{formik.errors.password}</p>
                 ) : null}
                 <Button type="submit" className="btn-success w-100">
                   Sign in
                 </Button>
-              {WrongMessage != "" ? (
+                {WrongMessage != "" ? (
                   <p className='error'>{WrongMessage}</p>
                 ) : null}
-            </Form >
-          )}
-          </Formik>
-      </Card.Body>
-    </Card>
+              </Form >
+            )}
+            </Formik>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
