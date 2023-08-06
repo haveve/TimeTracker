214333c3159dc3b74529cf74587c16b1bf53a4a6
Rainfront <Chudoha@gmail.com>
2023-08-06 22:47:53 +0000

    Added approvers setup to user profile, and others fixes

--- TimeTrackerFront/time-tracker-front/src/Components/Content.tsx
+++ TimeTrackerFront/time-tracker-front/src/Components/Content.tsx
@@ -1,24 +1,24 @@
 import '../Custom.css';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Login from '../Login/Features/Login';
-import { createBrowserRouter, RouterProvider, Outlet, redirect } from 'react-router-dom';
+import {createBrowserRouter, RouterProvider, Outlet, redirect} from 'react-router-dom';
 import ResetPassword from './ResetPassword';
 import Userslist from './UsersList';
 import AppNavbar from './Navbar';
 import UserDetails from './UserDetails';
 import CreateUser from './CreateUser';
 import UserProfile from './UserProfile';
-import { getTokenOrNavigate } from '../Login/Api/login-logout';
+import {getTokenOrNavigate} from '../Login/Api/login-logout';
 import TimeStatistic from "./TimeStatistic"
 import RequestResetPassword from "./RequestResetPassword";
 import UserRegistration from './UserRegistration';
 import Calendar from './Calendar';
 import ApproversSetup from "./ApproversSetup";
-import { useSelector } from 'react-redux';
-import { RootState } from '../Redux/store';
+import {useSelector} from 'react-redux';
+import {RootState} from '../Redux/store';
 import PermissionError from './PermissionError';
-import { RequestCurrentUser } from '../Redux/Requests/UserRequests';
-import { User } from '../Redux/Types/User';
+import {RequestCurrentUser} from '../Redux/Requests/UserRequests';
+import {User} from '../Redux/Types/User';
 
 const checkPermissions = (Permission: string, user: User) => {
   if (Permission === "ViewUsers" && user.viewUsers === false) {
@@ -34,62 +34,59 @@ const checkPermissions = (Permission: string, user: User) => {
 }
 const router = (user: User) => createBrowserRouter([
   {
-    element: <AppNavbar />,
+    element: <AppNavbar/>,
     loader: async () => getTokenOrNavigate(),
     children: [
       {
         path: "/",
-        element: <TimeStatistic />
+        element: <TimeStatistic/>
       },
       {
         path: "/Users",
         loader: async () => checkPermissions("ViewUsers", user),
-        element: <Userslist />,
+        element: <Userslist/>,
       },
       {
         path: "/Users/:userId",
         loader: async () => checkPermissions("UserDetails", user),
-        element: <UserDetails />
+        element: <UserDetails/>
       },
       {
         path: "/CreateUser",
         loader: async () => checkPermissions("CreateUsers", user),
-        element: <CreateUser />
+        element: <CreateUser/>
       },
       {
         path: "/User/:login",
-        element: <UserProfile />
+        element: <UserProfile/>
       },
       {
         path: "/Calendar",
-        element: <Calendar />
+        element: <Calendar/>
       },
-      {
-        path: "/ApproversSetup",
-        element: <ApproversSetup />
-      },
+
       {
         path: "/PermissionError",
-        element: <PermissionError />
+        element: <PermissionError/>
       }
     ]
   },
   {
     path: "/Login",
-    element: <Login />,
+    element: <Login/>,
     loader: async () => getTokenOrNavigate(true),
   },
   {
     path: "/ResetPassword",
-    element: <ResetPassword />
+    element: <ResetPassword/>
   },
   {
     path: "/RequestResetPassword",
-    element: <RequestResetPassword />
+    element: <RequestResetPassword/>
   },
   {
     path: "/UserRegistration",
-    element: <UserRegistration />
+    element: <UserRegistration/>
   }
 ])
 
@@ -99,7 +96,7 @@ function AppContent() {
 
   return (
     <div className='Content container-fluid p-0 h-100'>
-      <RouterProvider router={router(user)} />
+      <RouterProvider router={router(user)}/>
     </div>
   );
 }
--- TimeTrackerFront/time-tracker-front/src/Components/Navbar.tsx
+++ TimeTrackerFront/time-tracker-front/src/Components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Container,
   Col,
@@ -12,16 +12,16 @@ import {
   ListGroup,
   ListGroupItem
 } from "react-bootstrap";
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, Outlet } from 'react-router-dom';
+import {useSelector, useDispatch} from 'react-redux';
+import {Link, Outlet} from 'react-router-dom';
 import TimeTracker from './TimeTracker';
 
-import NotificationModalWindow, { MasssgeType } from './NotificationModalWindow';
-import { clearErroMassage as clearErroMassageTime } from '../Redux/Slices/TimeSlice';
-import { deleteCookie, getCookie } from '../Login/Api/login-logout';
-import { getCurrentUser } from '../Redux/epics';
-import { RootState } from '../Redux/store';
-import { clearErroMassage as clearErroMassageUserList } from '../Redux/Slices/UserSlice';
+import NotificationModalWindow, {MasssgeType} from './NotificationModalWindow';
+import {clearErroMassage as clearErroMassageTime} from '../Redux/Slices/TimeSlice';
+import {deleteCookie, getCookie} from '../Login/Api/login-logout';
+import {getCurrentUser} from '../Redux/epics';
+import {RootState} from '../Redux/store';
+import {clearErroMassage as clearErroMassageUserList} from '../Redux/Slices/UserSlice';
 
 import '../Custom.css';
 
@@ -39,9 +39,10 @@ function AppNavbar() {
     <Container fluid className='p-0 m-0 h-100'>
       <Navbar expand={false} className="bg-black">
         <Container fluid className='justify-content-start'>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false`} />
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false`}/>
           <Navbar.Brand as={Link} to={"/"} className='ms-1'>TimeTracker</Navbar.Brand>
-          <Nav.Link as={Link} to={user ? "/User/" + user.login : "/Login"} className='ms-auto'>{user ? user.login : "sign in"}</Nav.Link>
+          <Nav.Link as={Link} to={user ? "/User/" + user.login : "/Login"}
+                    className='ms-auto'>{user ? user.login : "sign in"}</Nav.Link>
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand-false`}
             aria-labelledby={`offcanvasNavbarLabel-expand-false`}
@@ -58,37 +59,36 @@ function AppNavbar() {
                 <ListGroup>
                   <ListGroup.Item action className='border-0 rounded-1 p-0 ps-2'>
                     <Nav.Link as={Link} to={"/Users"} className='m-0'>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-people me-1 mb-1" viewBox="0 0 16 16">
-                        <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                           className="bi bi-people me-1 mb-1" viewBox="0 0 16 16">
+                        <path
+                          d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z"/>
                       </svg>
                       Users
                     </Nav.Link>
                   </ListGroup.Item>
                   {user.cRUDUsers ?
-                    <ListGroup.Item action className='border-0 rounded-1 p-0 ps-2'><Nav.Link as={Link} to={"/CreateUser"} className='m-0'>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-plus me-1 mb-1" viewBox="0 0 16 16">
-                        <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-                        <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                    <ListGroup.Item action className='border-0 rounded-1 p-0 ps-2'><Nav.Link as={Link}
+                                                                                             to={"/CreateUser"}
+                                                                                             className='m-0'>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                           className="bi bi-person-plus me-1 mb-1" viewBox="0 0 16 16">
+                        <path
+                          d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                        <path fillRule="evenodd"
+                              d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
                       </svg>
                       Create user
                     </Nav.Link></ListGroup.Item>
                     : <></>
                   }
-                  {user.editPermiters ?
-                    <ListGroup.Item action className='border-0 rounded-1 p-0 ps-2'>
-                      <Nav.Link as={Link} to={"/ApproversSetup"} className='m-0'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-gear me-1 mb-1" viewBox="0 0 16 16">
-                          <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm.256 7a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Zm3.63-4.54c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382l.045-.148ZM14 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z" />
-                        </svg>
-                        Setup approvers
-                      </Nav.Link>
-                    </ListGroup.Item>
-                    : <></>
-                  }
+
                   <ListGroup.Item action className='border-0 rounded-1 p-0 ps-2'>
                     <Nav.Link as={Link} to={"/Calendar"} className='m-0'>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar me-1 mb-1" viewBox="0 0 16 16">
-                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                           className="bi bi-calendar me-1 mb-1" viewBox="0 0 16 16">
+                        <path
+                          d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
                       </svg>
                       Calendar
                     </Nav.Link>
@@ -98,13 +98,17 @@ function AppNavbar() {
             </Offcanvas.Body>
             <Nav className="justify-content-center ps-2 mb-3 flex flex-grow-1">
               <ListGroup className="justify-content-end flex flex-grow-1 pe-3 ">
-                <ListGroup.Item action className='border-0 rounded-1 p-0 ps-2'><Nav.Link as={Link} to={"/Login"} className='m-0'
-                  onClick={() => {
-                    deleteCookie("access_token");
-                  }}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-left me-1 mb-1" viewBox="0 0 16 16">
-                    <path fillRule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
-                    <path fillRule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
+                <ListGroup.Item action className='border-0 rounded-1 p-0 ps-2'><Nav.Link as={Link} to={"/Login"}
+                                                                                         className='m-0'
+                                                                                         onClick={() => {
+                                                                                           deleteCookie("access_token");
+                                                                                         }}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                       className="bi bi-box-arrow-left me-1 mb-1" viewBox="0 0 16 16">
+                    <path fillRule="evenodd"
+                          d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
+                    <path fillRule="evenodd"
+                          d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
                   </svg>
                   Logout</Nav.Link></ListGroup.Item>
               </ListGroup>
@@ -114,11 +118,13 @@ function AppNavbar() {
       </Navbar>
       <Row className='justify-content-end p-0 m-0 height-main h-100 '>
         <Col className='p-0 m-0 h-100 '>
-          <Outlet />
+          <Outlet/>
         </Col>
-      </Row >
-      <NotificationModalWindow isShowed={errorTime !== ""} dropMassege={() => dispatch(clearErroMassageTime())} messegeType={MasssgeType.Error}>{errorTime}</NotificationModalWindow>
-      <NotificationModalWindow isShowed={errorUserList !== ""} dropMassege={() => dispatch(clearErroMassageUserList())} messegeType={MasssgeType.Error}>{errorUserList}</NotificationModalWindow>
+      </Row>
+      <NotificationModalWindow isShowed={errorTime !== ""} dropMassege={() => dispatch(clearErroMassageTime())}
+                               messegeType={MasssgeType.Error}>{errorTime}</NotificationModalWindow>
+      <NotificationModalWindow isShowed={errorUserList !== ""} dropMassege={() => dispatch(clearErroMassageUserList())}
+                               messegeType={MasssgeType.Error}>{errorUserList}</NotificationModalWindow>
     </Container>
   );
 
